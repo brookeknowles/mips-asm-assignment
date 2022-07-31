7f8fbb6cfc54bc9bb537ae4b32d0fa5796a79d6f
Brooke Knowles <62309663+brookeknowles@users.noreply.github.com>
2022-07-31 21:39:16 +1200

    setup stack frame for task 3a

--- assignment1-3.asm
+++ assignment1-3.asm
@@ -32,14 +32,21 @@
 
   
   fibonacci:
-		# input argument ... ???
+		# input argument = register $t0
 
 		# set up the stack frame if needed 
-		
-	
+		addiu $sp, $sp, -8		# allocate 2 words on the stack (since int = 1 word = 4 bytes, and n < 12 means max 2 ints)
+		sw $ra, 0($sp)			# save ra register in the stack frame
+		sw $t0, 4($sp)			# save t0 register in the stack frame
 			# your code ... 
 			
   
 		# return the result 
 		
 			# your code ... 
+
+
+		# release the stack frame
+		lw $ra, 0($sp)			# restore ra register from stack frame
+		lw $t0, 4($sp)			# restore t0 register from stack frame
+		addiu $sp, $sp, 8 		# deallocate 2 words on the stack frame
