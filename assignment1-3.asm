a6589fcfbde45961b6e8ddfe6cd6970d8a09c082
Brooke Knowles <62309663+brookeknowles@users.noreply.github.com>
2022-08-07 20:50:24 +1200

    working for all except n=0

--- assignment1-3.asm
+++ assignment1-3.asm
@@ -11,6 +11,9 @@
 			.globl	    main
 	main: 
 
+		# initalise variables/consts
+		ori $s0, $0, 1 			# initalise i = 1
+
 		# print the initial message 
 		ori $v0, $0, 4 			# system call code for printing a string
 		la $a0, str0 			# print the message from str0
@@ -38,14 +41,14 @@
 		add $a0, $0, $s0 			# add i value to a0 register
 		jal fibonacci 				# go to fibonacci function
 
-		# print result
-		add $a0, $0, $v0
-		addi $v0, $0, 1
+		# print the resulting number
+		add $a0, $0, $v0			# fibonacci function saves result in v0 register, move to a0
+		addi $v0, $0, 1	 			# system call code for printing an integer
 		syscall
 
-		# print ','
-		addi $v0, $0, 4
-		la $a0, str2
+		# print print the comma and space between 2 numbers
+		addi $v0, $0, 4				# system call code for printing a string
+		la $a0, str2 				# print the message from str2
 		syscall
 
 		addi $s0, $s0, 1 			# add 1 to i
@@ -56,40 +59,39 @@
 		# input argument: $a0 (n in the c code)
 		
 		# set up the stack frame if needed 
-		addi $sp, $sp, -12
-		sw $ra, 0($sp)
-		sw $s0, 4($sp)
-		sw $s1, 8($sp)
+		addi $sp, $sp, -12			# allocate 12 bytes on the stack (since 1 int = 4 bytes, and n < 12 means max 2 ints will be used)
+		sw $ra, 0($sp)				# save ra register in the stack frame
+		sw $s0, 4($sp)				# save s0 register in the stack frame
+		sw $s1, 8($sp)				# save s1 register in the stack frame
 
-		add $s0, $a0, $zero
+		add $s0, $0, $a0			# put n into the s0 register
+		addi $t1, $0, 1				# initalise t1 register to 1 so can compare later
 
-		addi $t1, $zero, 1
-		beq $s0, $zero, return0
-		beq $s0, $t1, return1
+		beq $s0, $0, nIsZero		# if n == 0, go to nIsZero
+		beq $s0, $t1, nIsOneOrTwo 	# if n == 1, go to nIsOneOrTwo
 
-		addi $a0, $s0, -1
+		addi $a0, $s0, -1			# store n - 1 in a0 register
+		jal fibonacci				# call fibonacci function on n - 1
+		add $s1, $0, $v0     		# store result of fibonacci(n-1) in s1 register
 
-		jal fibonacci
+		addi $a0, $s0, -2			# store n - 2 in a0 register
+		jal fibonacci               # call fibonacci function on n - 2
+		add $v0, $v0, $s1       	# result of fibonacci(n-2) will be stored in v0 register, so add fibonacci(n-1) from s1 register
 
-		add $s1, $zero, $v0     # s1=fib(n-1)
-
-		addi $a0, $s0, -2
-
-		jal fibonacci               # v0=fib(n-2)
-
-		add $v0, $v0, $s1       # v0=fib(n-2)+$s1
-
-		exitfib:
-			lw $ra, 0($sp)       # read registers from stack
-			lw $s0, 4($sp)
-			lw $s1, 8($sp)
+		exitFib:
+			# release the stack frame and return the result
+			lw $ra, 0($sp)       	# restore ra register from stack frame
+			lw $s0, 4($sp)			# restore s0 register from stack frame
+			lw $s1, 8($sp)	       	# restore s1 register from stack frame
 			addi $sp, $sp, 12       # bring back stack pointer
-			jr $ra
+			jr $ra					# go to return address (where fibonacci was called last)
 
-		return1:
-			li $v0, 1
-			j exitfib
+		nIsOneOrTwo:
+			# second if statement of the C code: if (n == 1 || n == 2) return 1;
+			ori $v0, $0, 1			# set return value to 1
+			j exitFib				# sequence has been found so go to exitFib
 
-		return0:     
-			li $v0, 0
-			j exitfib
+		nIsZero:
+			# first if statement of the C code: if (n < 1) return 0;     
+			ori $v0, $0, 0			# set return value to 0
+			j exitFib				# sequence has been found so go to exitFib
